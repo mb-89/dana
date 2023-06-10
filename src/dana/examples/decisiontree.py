9692e8b40a835c6c1f955469df9fcf9cb6ac74cb
mb <ichbinmarkus@gmail.com>
2023-06-10 17:43:23 +0200

    added graphviz

--- src/dana/examples/decisiontree.py
+++ src/dana/examples/decisiontree.py
@@ -1,5 +1,7 @@
 """Basic example on how to plot decision tree with the dana backend."""
 
+import graphviz  # noqa: F401 # needed so that graphs can be drawn
+
 import dana as da  # noqa: F401 # to make the backend available, import dana
 import dana.exampledata as ex
 
